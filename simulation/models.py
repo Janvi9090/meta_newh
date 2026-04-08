fc22f15d25572c2e6b632fd2faae854e1bd8e0b5
Janvi9090 <maharnawarjanvi@gmail.com>
2026-04-08 22:06:59 +0530

    add all files from meta_h folder of local

--- simulation/models.py
+++ simulation/models.py
@@ -1,14 +1,7 @@
 from pydantic import BaseModel
 
 class Observation(BaseModel):
-    step: int
     concentration: float
-    metabolism_rate: float
-    last_dose: float
-    toxicity_flag: bool
 
 class Action(BaseModel):
     dose: float
-
-
-    
